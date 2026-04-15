7c7d9142c42d668ca28332cbff1780677dc79434
Bhavik Patel <bhavikpate2010@gmail.com>
2026-04-15 12:09:03 +0530

    Fix the server issue

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,16 +1,26 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 declare global {
   // eslint-disable-next-line no-var
   var prismaGlobal: PrismaClient;
 }
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is required to initialize Prisma");
+}
+
+const adapter = new PrismaPg(new Pool({ connectionString }));
+
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+    global.prismaGlobal = new PrismaClient({ adapter });
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma = global.prismaGlobal ?? new PrismaClient({ adapter });
 
 export default prisma;
